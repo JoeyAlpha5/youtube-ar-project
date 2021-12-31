560a023353fd611a05781c17cb04eb290084717a
Jalome <chjalome@gmail.com>
2021-12-31 16:56:21 +0200

    useful functions update

--- App.js
+++ App.js
@@ -1,55 +1,101 @@
 import React, {useState} from 'react';
-import {StyleSheet} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import {
   ViroARScene,
   ViroText,
   ViroConstants,
   ViroARSceneNavigator,
+  ViroBox,
+  ViroMaterials,
+  ViroAnimations,
+  Viro3DObject,
+  ViroAmbientLight
 } from '@viro-community/react-viro';
 
-const HelloWorldSceneAR = () => {
-  const [text, setText] = useState('Initializing AR...');
 
-  function onInitialized(state, reason) {
-    console.log('guncelleme', state, reason);
-    if (state === ViroConstants.TRACKING_NORMAL) {
-      setText('Hello World!');
-    } else if (state === ViroConstants.TRACKING_NONE) {
-      // Handle loss of tracking
+const InititalScene = (props)=>{
+  let data = props.sceneNavigator.viroAppProps;
+  ViroMaterials.createMaterials({
+    tv:{
+      diffuseTexture:require('./assets/tv/Old_Tv/TV_Body_material_Base_Color.png')
     }
-  }
+  })
 
-  return (
-    <ViroARScene onTrackingUpdated={onInitialized}>
-      <ViroText
-        text={text}
-        scale={[0.5, 0.5, 0.5]}
-        position={[0, 0, -1]}
-        style={styles.helloWorldTextStyle}
-      />
+  ViroAnimations.registerAnimations({
+    rotate:{
+      duration:2500,
+      properties:{
+        rotateY:'+=90'
+      }
+    }
+  })
+
+  return(
+    <ViroARScene>
+      <ViroAmbientLight color="#ffffff"/>
+      {data.object === "skull"?
+            <Viro3DObject
+              source={require('./assets/skull/12140_Skull_v3_L2.obj')}
+              position={[0,0,-5]}
+              scale={[0.05,0.05,0.05]}
+              rotation={[-45,50,40]}
+              type="OBJ"
+            />
+            :
+            <Viro3DObject
+              source={require('./assets/tv/Old_Tv/Old_Tv.obj')}
+              position={[0,0,-5]}
+              scale={[0.005,0.005,0.005]}
+              rotation={[-45,50,40]}
+              materials={["tv"]}
+              type="OBJ"
+            />
+
+      }
+
     </ViroARScene>
-  );
-};
+  )
+}
 
 export default () => {
+  const [object,setObject] = useState('tv');
   return (
-    <ViroARSceneNavigator
-      autofocus={true}
-      initialScene={{
-        scene: HelloWorldSceneAR,
-      }}
-      style={styles.f1}
-    />
+    <View style={styles.mainView}>
+      <ViroARSceneNavigator
+        initialScene={{
+          scene:InititalScene
+        }}
+        viroAppProps={{"object":object}}
+        style={{flex:1}}
+      />
+      <View style={styles.controlsView}>
+          <TouchableOpacity onPress={()=>setObject('skull')}>
+            <Text style={styles.text}>Display Skull</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={()=>setObject('tv')}>
+            <Text style={styles.text}>Display TV</Text>
+          </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
 var styles = StyleSheet.create({
-  f1: {flex: 1},
-  helloWorldTextStyle: {
-    fontFamily: 'Arial',
-    fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',
+  mainView:{
+    flex:1
   },
+  controlsView:{
+    width:'100%',
+    height:100,
+    backgroundColor:'#ffffff',
+    display:'flex',
+    flexDirection:'row',
+    justifyContent:'space-between'
+  },
+  text:{
+    margin:20,
+    backgroundColor:'#9d9d9d',
+    padding:10,
+    fontWeight:'bold'
+  }
 });
